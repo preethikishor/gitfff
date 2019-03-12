ec1791a33b35e523d77fd86f2bcb5470217e644c
preethikishor <30550110+preethikishor@users.noreply.github.com>
2019-03-12 23:31:12 +0530

    Update frr.java

--- src/fff/frr.java
+++ src/fff/frr.java
@@ -4,7 +4,7 @@ public class frr {
 
 	public static void main(String[] args) {
 		System.out.println("Exception while taking screenshotbbbdd ");
-		System.out.println("Exception while taking screenshot");
+		
 		System.out.println("Exception while taking screenshotrrrr");
 		
 	}
